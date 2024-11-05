ba300b899b39f0f27291f26744d78bd9848b1245
joshuamotoaki <dachshunduniverse@gmail.com>
2024-11-04 19:54:27 -0500

    Scaffold main page UI

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Header from "$lib/components/Header.svelte";
+    import ItemView from "$lib/components/ItemView.svelte";
+    import LeftBar from "$lib/components/LeftBar.svelte";
+</script>
+
+<div class="flex h-screen flex-col overflow-hidden">
+    <Header />
+    <div class="flex flex-1 bg-green-200">
+        <aside>
+            <LeftBar />
+        </aside>
+        <main class="flex-1 bg-blue-200">
+            <ItemView />
+        </main>
+    </div>
+</div>
