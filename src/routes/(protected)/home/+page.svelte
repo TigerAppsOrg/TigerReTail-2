b00b2ac1cf5b876f67a053695c4894918159cda7
joshuamotoaki <dachshunduniverse@gmail.com>
2024-12-02 21:13:59 -0500

    f

--- src/routes/(protected)/home/+page.svelte
+++ src/routes/(protected)/home/+page.svelte
@@ -1,13 +1,16 @@
 <script>
+    import Header from "$lib/components/Header.svelte";
     import ItemView from "$lib/components/ItemView.svelte";
     import LeftBar from "$lib/components/LeftBar.svelte";
 </script>
 
-<div class="flex">
-    <aside>
-        <LeftBar />
-    </aside>
-    <main class="flex-1 bg-blue-200">
-        <ItemView />
-    </main>
+<div class="flex h-screen flex-col overflow-hidden bg-green-200">
+    <div class="flex flex-1">
+        <aside>
+            <LeftBar />
+        </aside>
+        <main class="flex-1 bg-white">
+            <ItemView />
+        </main>
+    </div>
 </div>
