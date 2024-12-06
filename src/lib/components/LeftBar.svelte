b5b7163a7967ca324ec0eeb031d78d65fd40fd35
Erica Lee <el9280@princeton.edu>
2024-12-05 21:14:41 -0500

    First checkbox

--- src/lib/components/LeftBar.svelte
+++ src/lib/components/LeftBar.svelte
@@ -1,1 +1,15 @@
-<div>Hello</div>
+<script lang="ts">
+    import { Checkbox } from "$lib/components/ui/checkbox/index.js";
+    import { Label } from "$lib/components/ui/label/index.js";
+    let checked = $state(false);
+</script>
+
+<div class="flex items-center space-x-2">
+    <Checkbox id="terms" bind:checked aria-labelledby="terms-label" />
+    <Label
+        id="terms-label"
+        for="terms"
+        class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+        Prices
+    </Label>
+</div>
