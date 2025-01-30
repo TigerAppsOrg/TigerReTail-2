075fc748bd6dba3ce397ae987356d45f50f80c34
Joshua Lau <dachshunduniverse@gmail.com>
2025-01-29 21:57:09 -0500

    Scaffold landing page

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+    import GithubIcon from "$lib/icons/GithubIcon.svelte";
+</script>
+
+<footer
+    class="flex w-full items-center justify-center gap-4 p-2 text-xs text-gray-500">
+    <p>
+        &copy; 2025 <a
+            href="https://tigerapps.org"
+            target="_blank"
+            class="underline transition-colors hover:text-primary">TigerApps</a
+        >. All rights reserved.
+    </p>
+    <a
+        href="https://github.com/TigerAppsOrg/SakuyaSQL"
+        target="_blank"
+        class="text-gray-text transition-colors hover:text-primary">
+        <GithubIcon />
+    </a>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,52 @@
 <script>
+    import Footer from "$lib/components/Footer.svelte";
     import { Button } from "$lib/components/ui/button";
 </script>
 
-<Button href="/auth">Login</Button>
+<div class="flex min-h-screen flex-col">
+    <header class="grid h-12 w-full grid-cols-3 items-center px-4">
+        <div>
+            <h1>TigerReTail</h1>
+        </div>
+        <div class="justify-self-center">
+            <a href="#features" class="nav-link">Features</a>
+        </div>
+        <div class="justify-self-end">
+            <Button href="/auth">Sign in</Button>
+        </div>
+    </header>
+
+    <main></main>
+
+    <section id="features"></section>
+
+    <section id="about"></section>
+
+    <div class="flex-1"></div>
+    <Footer />
+</div>
+
+<style lang="postcss">
+    .nav-link {
+        position: relative;
+        text-decoration: none;
+    }
+
+    .nav-link::after {
+        content: "";
+        position: absolute;
+        width: 100%;
+        height: 2px;
+        bottom: 0;
+        left: 0;
+        transform: scaleX(0);
+        transform-origin: bottom right;
+        transition: transform 0.3s ease-out;
+        @apply bg-primary;
+    }
+
+    .nav-link:hover::after {
+        transform: scaleX(1);
+        transform-origin: bottom left;
+    }
+</style>
