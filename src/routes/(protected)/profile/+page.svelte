a75321ef801bb9befb5e237fe9b11bb106795089
joshuamotoaki <dachshunduniverse@gmail.com>
2025-02-27 21:03:58 -0500

    Better scaffold profile page

--- src/routes/(protected)/profile/+page.svelte
+++ src/routes/(protected)/profile/+page.svelte
@@ -1,15 +1,20 @@
 <script>
+    import { Button } from "$lib/components/ui/button";
+    import { ChevronLeft } from "lucide-svelte";
     import MyItems from "./MyItems.svelte";
 
     import ProfileInfo from "./ProfileInfo.svelte";
 </script>
 
 <div class="flex h-full flex-col">
-    <header class="border-b bg-blue-50 p-2">
-        <button> Back to Image Gallery </button>
-    </header>
-    <div class="flex flex-1 flex-col">
-        <h2 class="mx-auto mb-2 w-[90%] border-b">Profile</h2>
+    <div class="cont flex flex-1 flex-col">
+        <div class="my-4 flex items-center justify-between border-b p-2">
+            <h2 class="text-xl font-semibold">Profile</h2>
+            <Button href="/home" variant="outline">
+                <ChevronLeft class="mr-1" />
+                Back to Home
+            </Button>
+        </div>
         <div class="flex flex-1 gap-4">
             <div>
                 <ProfileInfo />
