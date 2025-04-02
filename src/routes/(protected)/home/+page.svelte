4c50ffdaab67c73ecbb4fe8cef711a6b2e5be0e9
Joshua Lau <dachshunduniverse@gmail.com>
2025-04-02 15:47:37 -0400

    Main page header

--- src/routes/(protected)/home/+page.svelte
+++ src/routes/(protected)/home/+page.svelte
@@ -1,17 +1,77 @@
-<script>
+<script lang="ts">
+    import Button from "$lib/components/ui/button/button.svelte";
+    import Input from "$lib/components/ui/input/input.svelte";
+    import Label from "$lib/components/ui/label/label.svelte";
+    import Checkbox from "$lib/components/ui/checkbox/checkbox.svelte";
+    import * as Select from "$lib/components/ui/select/";
+
+    import { SearchIcon } from "lucide-svelte";
+
     import ItemView from "./ItemView.svelte";
     import LeftBar from "./LeftBar.svelte";
+    import { sortValues, type SortValue } from "$lib/utils";
 
     let { data } = $props();
+
+    let isCompact = $state(false);
+    let sortBy = $state<SortValue>("date_new");
+    let sortByDisplay = $derived(
+        sortValues.find((s) => s.value === sortBy)!.label
+    );
 </script>
 
 <div class="flex flex-col overflow-hidden">
-    <div class="flex flex-1">
+    <header class="border-b border-std p-2 pb-4 flex flex-col cont mb-6">
+        <div class="flex items-center justify-between w-full gap-16">
+            <div>
+                <h2 class="text-lg font-bold">Items for Sale</h2>
+                <p class="text-sm">
+                    Find items to purchase here. Check out our FAQ for more.
+                </p>
+            </div>
+            <div class="flex items-center flex-1 max-w-96">
+                <Input type="text" placeholder="Search..." />
+                <Button type="submit"><SearchIcon class="size-4" /></Button>
+            </div>
+        </div>
+        <div class="flex items-center justify-between mt-4">
+            <div class="flex items-center space-x-2">
+                <Checkbox
+                    id="compact"
+                    bind:checked={isCompact}
+                    aria-labelledby="compact" />
+                <Label
+                    id="compact"
+                    for="compact"
+                    class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                    Compact View
+                </Label>
+            </div>
+
+            <div>
+                <Select.Root type="single" name="sortBy" bind:value={sortBy}>
+                    <Select.Trigger>
+                        {sortByDisplay}
+                    </Select.Trigger>
+                    <Select.Content>
+                        {#each sortValues as option}
+                            <Select.Item
+                                value={option.value}
+                                label={option.label}>
+                                {option.label}
+                            </Select.Item>
+                        {/each}
+                    </Select.Content>
+                </Select.Root>
+            </div>
+        </div>
+    </header>
+    <section class="flex flex-1 cont">
         <aside>
             <LeftBar />
         </aside>
         <main class="flex-1 bg-white">
             <ItemView />
         </main>
-    </div>
+    </section>
 </div>
