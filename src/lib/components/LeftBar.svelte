2be016a86a5d85dcebd5bcdc659725357a8c92e8
Nicholas Vickery <nv9344@princeton.edu>
2025-04-03 20:59:14 -0400

    Used loop to display values, added some functionality features for later use

--- src/lib/components/LeftBar.svelte
+++ src/lib/components/LeftBar.svelte
@@ -29,27 +29,38 @@
         "poor"
     ];
 
+  let selectedCategories: { [key: string]: boolean } = {};
+  let selectedConditions: { [key: string]: boolean } = {};
+
+  categories.forEach(category => selectedCategories[category] = false);
+  conditions.forEach(condition => selectedConditions[condition] = false);
+
+  function getSelectedItems() {
+    const activeCategories = Object.keys(selectedCategories).filter(key => selectedCategories[key]);
+    const activeConditions = Object.keys(selectedConditions).filter(key => selectedConditions[key]);
+    console.log("Selected Categories:", activeCategories);
+    console.log("selected Conditions:", activeConditions);
+  }
   </script>
 
-<div>Categories</div>
-<div> <Checkbox/> accessories </div>
-<div> <Checkbox/> beauty supplies </div>
-<div> <Checkbox/> clothing </div>
-<div> <Checkbox/> dorm essentials </div>
-<div> <Checkbox/> event tickets </div>
-<div> <Checkbox/> food </div>
-<div> <Checkbox/> furniture </div>
-<div> <Checkbox/> housing </div>
-<div> <Checkbox/> other </div>
-<div> <Checkbox/> school supplies </div>
-<div> <Checkbox/> services </div>
-<div> <Checkbox/> tech </div>
-<div> <Checkbox/> textbooks </div>
-<div> <Checkbox/> transportation </div>
+<div>
+    <h2 class="mb-4">Categories</h2>
+    {#each categories as category}
+      <div>
+        <Checkbox
+        />
+        {category}
+      </div>
+    {/each}
+    </div>
 
-<div>Conditions</div>
-<div> <Checkbox/> new </div>
-<div> <Checkbox/> like new </div>
-<div> <Checkbox/> gently loved </div>
-<div> <Checkbox/> well loved </div>
-<div> <Checkbox/> poor </div>
+<div class="mt-8">
+    <h2 class="mb-4">Conditions</h2>
+    {#each conditions as condition}
+      <div>
+        <Checkbox
+        />
+        {condition}
+      </div>
+    {/each}
+    </div>
